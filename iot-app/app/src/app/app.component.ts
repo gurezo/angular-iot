2db4b933117c31e0f5b09cfa34260fa40cda929e
gurezo <akihiko.kigure@gmail.com>
2021-01-16 17:31:25 +0900

    code add.

--- iot-app/app/src/app/app.component.ts
+++ iot-app/app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngiot-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  isLedBlink = false;
+
+  title = '停止中';
+
+  ngOnInit() {
+    this.isLedBlink = false;
+  }
+
+  onClick() {
+    this.isLedBlink = !this.isLedBlink;
+    this.title = this.isLedBlink ? 'Lチカなう' : '停止中';
+    console.log('this.isLedBlink: ', this.isLedBlink);
+
+    // FIXME: rxjs で、ストリームを作成して、流す
+  }
 }
