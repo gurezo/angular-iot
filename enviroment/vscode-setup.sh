cd73541054dcca851e9ca3287394c5c9d5a1afcd
gurezo <akihiko.kigure@gmail.com>
2021-02-13 23:43:35 +0900

    sh modified.

--- enviroment/vscode-setup.sh
+++ enviroment/vscode-setup.sh
@@ -6,3 +6,5 @@ set -e
 # VS code のインストール
 wget -O /tmp/code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64'
 sudo apt install -y /tmp/code.deb
+
+rm -rf /tmp/code.deb
