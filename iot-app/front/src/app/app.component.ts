50003d0d206e46890a49210e0a87c7e8231ceaee
gurezo <akihiko.kigure@gmail.com>
2021-12-13 23:08:44 +0900

    method rename

--- iot-app/front/src/app/app.component.ts
+++ iot-app/front/src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { requestGPIOAccess } from '../../node_modules/node-web-gpio';
 
 @Component({
   selector: 'ngiot-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   isLedBlink = false;
+  requestGPIOAccess = requestGPIOAccess;
 
   title = '停止中';
 
-  async ngOnInit() {
+  ngOnInit() {
     this.isLedBlink = false;
   }
 
-  onClick() {
+  async onClickAuto() {
     this.isLedBlink = !this.isLedBlink;
     this.title = this.isLedBlink ? 'Lチカなう' : '停止中';
+    const gpioAccess = await requestGPIOAccess();
+    const port = gpioAccess.ports.get(26);
+    await port?.export('out');
+    setInterval(async () => await port?.export('out'), 1000);
     console.log('this.isLedBlink: ', this.isLedBlink);
+  }
+
+  onClickManual() {
+    //
   }
 }
