33521938d7fd68cf4945eb2347a79c1d5fe381b8
gurezo <akihiko.kigure@gmail.com>
2021-12-13 23:08:54 +0900

    prettier change

--- iot-app/front/src/app/app.module.ts
+++ iot-app/front/src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GpioService } from './gpio.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    GpioService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [GpioService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
