d5095bd0f11f48b49c4b82d7395f907b187ce957
gurezo <akihiko.kigure@gmail.com>
2021-12-16 22:50:13 +0900

    service 削除

--- iot-app/front/src/app/app.module.ts
+++ iot-app/front/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GpioService } from './gpio.service';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [GpioService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
