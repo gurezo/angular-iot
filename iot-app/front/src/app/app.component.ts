ce0e6c08415415302ec562761689c05a46d9d815
gurezo <akihiko.kigure@gmail.com>
2021-12-16 22:50:25 +0900

    リファクタリング

--- iot-app/front/src/app/app.component.ts
+++ iot-app/front/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// declare var require: any;
-// const requestGPIOAccess = require('node-web-gpio');
-import { requestGPIOAccess } from 'node-web-gpio';
+import { GPIOAccess, GPIOPort, requestGPIOAccess } from 'node-web-gpio';
 
 @Component({
   selector: 'ngiot-root',
@@ -9,26 +7,53 @@ import { requestGPIOAccess } from 'node-web-gpio';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  isLedBlink = false;
+  /** requestGPIOAccess 割当 */
   requestGPIOAccess = requestGPIOAccess;
+  /** GPIOAccess 定義 */
+  gpioAccess: GPIOAccess | undefined;
+  /** GPIOPort 定義 */
+  port: GPIOPort | undefined;
 
+  /** ボタンタイトル */
   title = '停止中';
+  /** LED点灯フラグ */
+  isLedBlink = false;
+  /** setInterval 用ID */
+  nodeInterval: NodeJS.Timeout | null;
 
-  ngOnInit() {
+  constructor() {
+    this.nodeInterval = null;
+  }
+
+  async ngOnInit() {
     this.isLedBlink = false;
+    this.gpioAccess = await requestGPIOAccess();
+    // GPIOピン割当
+    this.port = this.gpioAccess.ports.get(26);
+    // GPIOピン出力モード
+    await this.port?.export('out');
   }
 
   async onClickAuto() {
-    this.isLedBlink = !this.isLedBlink;
-    this.title = this.isLedBlink ? 'Lチカなう' : '停止中';
-    const gpioAccess = await requestGPIOAccess();
-    const port = gpioAccess.ports.get(26);
-    await port?.export('out');
-    setInterval(async () => await port?.export('out'), 1000);
-    console.log('this.isLedBlink: ', this.isLedBlink);
+    this.isLedBlink ? this.stopNodeInterval() : this.startNodeInterval();
   }
 
   onClickManual() {
     //
+  }
+
+  private startNodeInterval() {
+    this.nodeInterval = setInterval(async () => {
+      this.isLedBlink = !this.isLedBlink;
+      this.title = this.isLedBlink ? 'Lチカなう' : '停止中';
+      await this.port?.write(1);
+    }, 1000);
+  }
+
+  private stopNodeInterval() {
+    if (this.nodeInterval !== null) {
+      clearInterval(this.nodeInterval);
+    }
+    this.isLedBlink = false;
   }
 }
