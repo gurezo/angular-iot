06739b7283187198412dad426de268a95104a053
akihiko.kigure <akihiko.kigure@gmail.com>
2022-04-02 16:36:51 +0900

    structure change

--- iot-app/front/src/app/app.component.ts
+++ iot-app/front/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GPIOAccess, GPIOPort, requestGPIOAccess } from 'node-web-gpio';
 
 @Component({
   selector: 'ngiot-root',
@@ -7,13 +6,6 @@ import { GPIOAccess, GPIOPort, requestGPIOAccess } from 'node-web-gpio';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  /** requestGPIOAccess 割当 */
-  requestGPIOAccess = requestGPIOAccess;
-  /** GPIOAccess 定義 */
-  gpioAccess: GPIOAccess | undefined;
-  /** GPIOPort 定義 */
-  port: GPIOPort | undefined;
-
   /** ボタンタイトル */
   labelBlink = '';
   /** LED点灯フラグ */
@@ -32,8 +24,6 @@ export class AppComponent implements OnInit {
   async ngOnInit() {
     this.isLedBlink = false;
     this.isAuto = true;
-    this.labelBlink = 'LED消灯';
-    await this.connectRaspberryPi();
     console.log('%cStop!', 'font-size: 100px; color: red; font-weight: bold');
   }
 
@@ -70,25 +60,12 @@ export class AppComponent implements OnInit {
    */
   private async connectRaspberryPi() {
     this.isConnectError = false;
-    try {
-      this.gpioAccess = await requestGPIOAccess();
-      // GPIOピン割当
-      this.port = this.gpioAccess.ports.get(26);
-      // GPIOピン出力モード
-      await this.port?.export('out');
-    } catch (error) {
-      this.isConnectError = true;
-    }
   }
 
   /**
    * LED点灯処理
    */
-  private async blinkLED() {
-    this.isLedBlink = !this.isLedBlink;
-    this.labelBlink = this.isLedBlink ? 'LED点灯中' : 'LED消灯';
-    this.isLedBlink ? await this.port?.write(1) : await this.port?.write(0);
-  }
+  private async blinkLED() {}
 
   /**
    * 自動・手動モード変更
@@ -114,9 +91,6 @@ export class AppComponent implements OnInit {
    * LED自動点灯タイマー停止
    */
   private stopNodeInterval() {
-    if (this.nodeInterval !== null) {
-      clearInterval(this.nodeInterval);
-    }
     this.isLedBlink = false;
   }
 }
